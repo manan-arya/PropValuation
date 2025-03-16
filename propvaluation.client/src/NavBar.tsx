b11489d481c22a13a6a0ef69aef0e0c8010130ee
Manan Arya <mananarya18@gmail.com>
2025-03-16 21:26:39 +0530

    Deploy Test

--- propvaluation.client/src/NavBar.tsx
+++ propvaluation.client/src/NavBar.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function AppNavbar() {
     return (
         <Navbar className="navbar-custom" expand="lg" sticky="top">
-            <Navbar.Brand href="#home">Weather App</Navbar.Brand>
+            <Navbar.Brand href="#home">PropValutaion</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
